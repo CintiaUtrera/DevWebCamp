6724fec5ec812a3eeacb94df61197d135eddb701
Cintia <cintiaa_utrera@outlook.es>
2024-01-03 12:52:02 -0300

    creando tabla de usuario

--- views/auth/registro.php
+++ views/auth/registro.php
@@ -2,18 +2,20 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Regístrate en DevWebCamp</p>
 
-    <form class="formulario" action="">
+    <?php require_once __DIR__ . '/../templates/alertas.php'; ?>
+
+    <form class="formulario" action="/registro" method="POST">
     <div class="formulario__campo">
             <label for="nombre" class="formulario__label">Nombre</label>
-            <input type="text" class="formulario__input"  id="nombre" name="nombre" />
+            <input type="text" class="formulario__input"  id="nombre" name="nombre" value="<?php echo $usuario->nombre; ?>" />
         </div>
         <div class="formulario__campo">
             <label for="nombre" class="formulario__label">Apellido</label>
-            <input type="text" class="formulario__input"  id="apellido" name="apellido" />
+            <input type="text" class="formulario__input"  id="apellido" name="apellido" value="<?php echo $usuario->apellido; ?>" />
         </div>
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
-            <input type="email" class="formulario__input"  id="email" name="email" />
+            <input type="email" class="formulario__input"  id="email" name="email" value="<?php echo $usuario->email; ?>" />
         </div>
         <div class="formulario__campo">
             <label for="password" class="formulario__label">Password</label>
