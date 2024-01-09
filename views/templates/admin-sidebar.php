b42554319c26d20cc52195b295fc7d7e994273cc
Cintia <cintiaa_utrera@outlook.es>
2024-01-08 21:50:33 -0300

    resaltar pagina actual

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,6 +1,6 @@
 <aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
-        <a href="/admin/dashboard" class="dashboard__enlace">
+        <a href="/admin/dashboard" class="dashboard__enlace ">
             <i class="fa-solid fa-house"></i>
             <span class="dashboard__menu-texto">Inicio</span>
         </a>
