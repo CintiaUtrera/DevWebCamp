df15b99e34508bdee56467e3a0d5aff5042ab85e
Cintia <cintiaa_utrera@outlook.es>
2024-01-11 18:41:51 -0300

    añadiendo campos extras

--- views/admin/ponentes/formulario.php
+++ views/admin/ponentes/formulario.php
@@ -10,11 +10,29 @@
         <input type="text" class="formulario__input" id="apellido" name="apellido" placeholder="Apellido Ponente" value="<?php echo $ponente->apellido ?? ''; ?>" />
     </div>
     <div class="formulario__campo">
-        <label for="nombre" class="formulario__label">Nombre</label>
-        <input type="text" class="formulario__input" id="nombre" name="nombre" placeholder="Nombre Ponente" value="<?php echo $ponente->nombr ?? ''; ?>" />
+        <label for="ciudad" class="formulario__label">Ciudad</label>
+        <input type="text" class="formulario__input" id="ciudad" name="ciudad" value="<?php echo $ponente->ciudad ?? ''; ?>" />
     </div>
     <div class="formulario__campo">
-        <label for="nombre" class="formulario__label">Nombre</label>
-        <input type="text" class="formulario__input" id="nombre" name="nombre" placeholder="Nombre Ponente" value="<?php echo $ponente->nombr ?? ''; ?>" />
+        <label for="pais" class="formulario__label">Pais</label>
+        <input type="text" class="formulario__input" id="pais" name="pais" value="<?php echo $ponente->pais ?? ''; ?>" />
+    </div>
+    <div class="formulario__campo">
+        <label for="imagen" class="formulario__label">Imagen</label>
+        <input type="file" class="formulario__input formulario__input--file" id="imagen" name="imagen" />
+    </div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Información Extra</legend>
+
+    <div class="formulario__campo">
+        <label for="tags_input" class="formulario__label">Áreas de Experiencias(Separadas por Coma)</label>
+        <input type="text" class="formulario__input" id="tags_input" placeholder="Ej: Node.js, PHP, Laravel, UX / UI" />
+
+        <div id="tags" class="formulario__listado">
+            <input type="hidden" name="tags" value="<?php echo $ponente->tags ?? ''; ?>">
+        </div>
+    
     </div>
 </fieldset>
