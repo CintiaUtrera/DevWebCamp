d639210b5c62b5203f4b1f1ad0507e82c423c684
Cintia <cintiaa_utrera@outlook.es>
2024-01-16 10:11:59 -0300

    listando ponentes

--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -9,7 +9,7 @@
 <div class="dashboard__contenedor">
     <?php if(!empty($ponentes)) { ?>
         <table class="table">
-            <thead>
+            <thead class="table__thead">
                 <tr>
                     <th class="table__th" scope="col">Nombre</th>
                     <th class="table__th" scope="col">Ubicación</th>
@@ -21,8 +21,8 @@
                     <tr class="table__tr">
                         <td class="table__td"><?php echo $ponente->nombre . " " . $ponente->apellido; ?></td>
                         <td class="table__td"><?php echo $ponente->ciudad . ", " . $ponente->pais; ?></td>
-                        <td class="table__td--acciones"><a href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>"><i class="fa-solid fa-user-pen"></i> Editar</a>
-                            <form class="table__formulario" action=""><button type="submit"><i class="fa-solid fa-circle-xmark"></i> Eliminar</button></form>
+                        <td class="table__td--acciones"><a class="table__accion table__accion--editar" href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>"><i class="fa-solid fa-user-pen"></i> Editar</a>
+                            <form class="table__formulario" action=""><button class="table__accion table__accion--eliminar" type="submit"><i class="fa-solid fa-circle-xmark"></i> Eliminar</button></form>
                         </td>
                     </tr>
                 <?php } ?>
