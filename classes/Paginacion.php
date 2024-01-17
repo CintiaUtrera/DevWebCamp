c7cd99c16045915eb0d0afd15d09808a47a9328d
Cintia <cintiaa_utrera@outlook.es>
2024-01-17 20:21:04 -0300

    creando clase paginacion

--- classes/Paginacion.php
+++ classes/Paginacion.php
@@ -5,4 +5,10 @@ class Paginacion{
     public $pagina_actual;
     public $registro_por_pagina;
     public $total_registro;
+
+
+    public function __construct($pagina_actual = 1, $registro_por_pagina = 10, $total_registro = 0 )
+    {
+        
+    }
 }
