8b7e4e82c56b2a4616737415336b5928d0271642
Cintia <cintiaa_utrera@outlook.es>
2024-02-05 11:52:40 -0300

    añadiendo info a la tabla

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -7,6 +7,7 @@ use Model\Categoria;
 use Model\Dia;
 use Model\Evento;
 use Model\Hora;
+use Model\Ponente;
 use MVC\Router;
 
 class EventosController{
@@ -26,6 +27,9 @@ class EventosController{
 
         foreach($eventos as $evento){
             $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
         }
 
         $router->render('admin/eventos/index', [
