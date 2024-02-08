a9274853b3d2b996d4fc3e3bd5ac9834bfc36ffb
Cintia <cintiaa_utrera@outlook.es>
2024-02-08 10:24:48 -0300

    creando paginas

---
+++ controllers/PaginasController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class PaginasController {
+    public static function index(Router $router) {
+
+        $router->render('paginas/index', [
+            'titulo' => 'Inicio'
+
+        ]);
+    }
+
+
+    public static function evento(Router $router) {
+
+        $router->render('paginas/devwebcamp', [
+            'titulo' => 'Sobre WebDevCamp'
+
+        ]);
+    }
+
+
+    public static function paquetes(Router $router) {
+
+        $router->render('paginas/paquetes', [
+            'titulo' => 'Paquetes WebDevCamp'
+
+        ]);
+    }
+
+
+    public static function conferencias(Router $router) {
+
+        $router->render('paginas/conferencias', [
+            'titulo' => 'Conferencias & Workshops'
+
+        ]);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ use Controllers\EventosController;
 use Controllers\RegalosController;
 use Controllers\PonentesController;
 use Controllers\DashboardController;
+use Controllers\PaginasController;
 use Controllers\RegistradosController;
 
 $router = new Router();
@@ -65,6 +66,13 @@ $router->get('/admin/registrados', [RegistradosController::class, 'index']);
 
 
 $router->get('/admin/regalos', [RegalosController::class, 'index']);
+
+// Area publica
+$router->get('/', [PaginasController::class, 'index']);
+$router->get('/devwebcamp', [PaginasController::class, 'evento']);
+$router->get('/paquetes', [PaginasController::class, 'paquetes']);
+$router->get('/workshops-conferencias', [PaginasController::class, 'conferencias']);
+
 
 
 $router->comprobarRutas();
