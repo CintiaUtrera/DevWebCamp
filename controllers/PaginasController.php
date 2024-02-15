0a2af26234cf83a9d28c0dab5964160ac41882a8
Cintia <cintiaa_utrera@outlook.es>
2024-02-15 12:02:17 -0300

    ordenar lo eventos en el backend

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -2,7 +2,12 @@
 
 namespace Controllers;
 
+use Model\Dia;
+use Model\Hora;
 use MVC\Router;
+use Model\Evento;
+use Model\Ponente;
+use Model\Categoria;
 
 class PaginasController {
     public static function index(Router $router) {
@@ -33,7 +38,30 @@ class PaginasController {
 
 
     public static function conferencias(Router $router) {
+        $eventos = Evento::ordenar('hora_id', 'ASC');
         
+        $eventos_formateados = [];
+        foreach($eventos as $evento){
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+            
+            if($evento->dia_id === "1" && $evento->categoria_id === "1"){
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "1"){
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+            if($evento->dia_id === "1" && $evento->categoria_id === "2"){
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "2"){
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+        }
 
         $router->render('paginas/conferencias', [
             'titulo' => 'Conferencias & Workshops'
--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -138,6 +138,13 @@ class ActiveRecord {
         return array_shift( $resultado ) ;
     }
 
+    //Retornar los registros por orden
+    public static function ordenar($columna, $orden){
+        $query = "SELECT * FROM " . static::$tabla . " ORDER BY {$columna} {$orden} ";
+        $resultado = self::consultarSQL($query);
+        return $resultado;
+    }
+
     // Busqueda Where con multiples opciones
     public static function whereArray($array = []) {
         $query = "SELECT * FROM " . static::$tabla . " WHERE ";
