34e23d9c276bab5df5dbf5e651f7add0e0b79669
Cintia <cintiaa_utrera@outlook.es>
2024-02-19 17:02:53 -0300

    resaltar pagina actual

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,8 +1,15 @@
 <header class="header">
     <div class="header__contenedor">
         <nav class="header__navegacion">
+            <?php if(is_auth()) { ?>
+                <a href="<?php echo is_admin() ? '/admin/dashboard' : '/finalizar-registro'; ?>" class="header__enlace">Administrar</a>
+                <form method="POST" action="/logout"  class="header__form">
+                    <input type="submit" value="Cerrar Sesión" class="header__submit">
+                </form>
+            <?php } else { ?>
             <a href="/registro" class="header__enlace">Registro</a>
             <a href="/login" class="header__enlace">Iniciar Sesión</a>
+            <?php } ?>
         </nav>
         <div class="header__contenido">
             <a href="/">
@@ -18,10 +25,10 @@
     <div class="barra__contenido">
         <a href="/"><h2 class="barra__logo" >&#60;DevWebCamp/></h2></a>
         <nav class="navegacion">
-            <a class="navegacion__enlace" href="/devwebcamp">Evento</a>
-            <a class="navegacion__enlace" href="/paquetes">Paquetes</a>
-            <a class="navegacion__enlace" href="/workshops-conferencias">Workshops / Conferencias</a>
-            <a class="navegacion__enlace" href="/registro">Comprar Pase</a>
+            <a href="/devwebcamp" class="navegacion__enlace <?php echo pagina_actual('/devwebcamp') ? 'navegacion__enlace--actual' : ''; ?>">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace <?php echo pagina_actual('/paquetes') ? 'navegacion__enlace--actual' : ''; ?>">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace <?php echo pagina_actual('/workshops-conferencias') ? 'navegacion__enlace--actual' : ''; ?>">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace <?php echo pagina_actual('/registro') ? 'navegacion__enlace--actual' : ''; ?>">Comprar Pase</a>
         </nav>
     </div>
 </div>
