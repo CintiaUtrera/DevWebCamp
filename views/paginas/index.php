09d832759debb6b9f382df0a637bfdc25f587243
Cintia <cintiaa_utrera@outlook.es>
2024-03-01 12:48:37 -0300

    creando boton para registro presencial

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -114,13 +114,13 @@
             <p class="boleto__precio">$2000</p>
         </div>
 
-        <div <?php aos_animacion(); ?> class="boleto boleto--virtual">
+        <div class="boleto boleto--virtual">
             <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
             <p class="boleto__plan">Virtual</p>
             <p class="boleto__precio">$1000</p>
         </div>
 
-        <div <?php aos_animacion(); ?> class="boleto boleto--gratis">
+        <div class="boleto boleto--gratis">
             <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
             <p class="boleto__plan">Gratis</p>
             <p class="boleto__precio">Gratis - $0</p>
--- views/registro/crear.php
+++ views/registro/crear.php
@@ -26,6 +26,12 @@
                 <li class="paquete_elemento">Comida y Bebida</li>
                 <p class="paquete__precio">$2000</p>
             </ul>
+
+            <div id="smart-button-container">
+                <div style="text-align: center;">
+                    <div id="paypal-button-container"></div>
+                </div>
+            </div>
         </div>
 
         <div <?php aos_animacion(); ?> class="paquete">
@@ -42,3 +48,47 @@
 
     </div>
 </main>
+
+
+<script src="https://www.paypal.com/sdk/js?client-id=AdbomR8r-nX7Uw6ROhWaD56wh3R0syXddGCqglPTgmHMBxUhcRHxLCTkfd4X3KeE90FMGHqh8y5Q7ETz&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
+ 
+<script>
+    function initPayPalButton() {
+      paypal.Buttons({
+        style: {
+          shape: 'rect',
+          color: 'blue',
+          layout: 'vertical',
+          label: 'pay',
+        },
+ 
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{"description":"1","amount":{"currency_code":"USD","value":199}}]
+          });
+        },
+ 
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
+ 
+            // Full available details
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+ 
+            // Show a success message within this page, e.g.
+            const element = document.getElementById('paypal-button-container');
+            element.innerHTML = '';
+            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+ 
+            // Or go to another URL:  actions.redirect('thank_you.html');
+            
+          });
+        },
+ 
+        onError: function(err) {
+          console.log(err);
+        }
+      }).render('#paypal-button-container');
+    }
+ 
+  initPayPalButton();
+</script>
