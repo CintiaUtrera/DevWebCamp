dd26a7bb2066439e0520ca6f47316dc69e35cf1b
Cintia <cintiaa_utrera@outlook.es>
2024-03-08 20:47:54 -0300

    ruta para regalos

--- models/Regalo.php
+++ models/Regalo.php
@@ -2,22 +2,14 @@
 
 namespace Model;
 
-class Regalo extends ActiveRecord{
+class Regalo extends ActiveRecord {
     protected static $tabla = 'regalos';
     protected static $columnasDB = ['id', 'nombre'];
 
-    public $id;
-    public $nombre;
-    
-
-    
     public function __construct($args = [])
     {
         $this->id = $args['id'] ?? null;
         $this->nombre = $args['nombre'] ?? '';
-        
     }
 
-
-    
 }
--- public/index.php
+++ public/index.php
@@ -59,6 +59,7 @@ $router->post('/admin/eventos/eliminar', [EventosController::class, 'eliminar'])
 $router->get('/api/eventos-horario', [APIEventos::class, 'index']);
 $router->get('/api/ponentes', [APIPonentes::class, 'index']);
 $router->get('/api/ponente', [APIPonentes::class, 'ponente']);
+$router->get('/api/regalos', [APIPonentes::class, 'index']);
 
 
 $router->get('/admin/registrados', [RegistradosController::class, 'index']);
